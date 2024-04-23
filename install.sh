d8b2be92473682fe85dff96c05fd1b2ebfc52e9e
Thomas WIlliam <152791626+thomasDwilliam@users.noreply.github.com>
2024-04-23 11:22:27 +0630

    Add files via upload

--- install.sh
+++ install.sh
@@ -14,11 +14,7 @@ ngrok config add-authtoken $token
 echo "Enter telegram bot token"
 read bot_token
 
-export botToken=$bot_token
+export botToken="$bot_token"
 docker run -d -p 8081:8081 --name=telegram-bot-api --restart=always -v telegram-bot-api-data:/var/lib/telegram-bot-api -e TELEGRAM_API_ID=7784110 -e TELEGRAM_API_HASH=f81b6478f985c1283fa8c4847d1860ec -e TELEGRAM_LOCAL=1 -e TELEGRAM_STAT=1 -p 8082:8082 aiogram/telegram-bot-api:latest
-curl http://172.17.0.2:8081/bot$botToken/setWebhook?url=https://factual-routinely-guppy.ngrok-free.app
-php -S localhost:8080 & ngrok http --domain=factual-routinely-guppy.ngrok-free.app http://localhost:8080
-
-
-
-
+curl http://172.17.0.2:8081/bot"$botToken"/setWebhook?url=https://factual-routinely-guppy.ngrok-free.app
+php -S localhost:8080 --bind 0.0.0.0:8080 & ngrok http --domain=factual-routinely-guppy.ngrok-free.app http://localhost:8080 & curl http://172.17.0.2:8081/bot"$botToken"/setWebhook?url=https://factual-routinely-guppy.ngrok-free.app

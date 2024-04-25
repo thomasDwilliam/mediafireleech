ea17a4baec10153bb34689ec65602beb8258cffc
Thomas WIlliam <152791626+thomasDwilliam@users.noreply.github.com>
2024-04-25 20:40:55 +0630

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,52 +5,80 @@ GREEN='\033[0;32m'
 # ANSI escape code to reset color
 NC='\033[0m'
 
-# Install required packages
-apk add php php-curl php-dom php-openssl python3 ffmpeg
+# Function to install ngrok
+install_ngrok() {
+    wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && \
+    tar -xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin && \
+    rm -rf ngrok-v3-stable-linux-amd64.tgz
+}
 
-# Copy PHP files to destination
-cp index.php /root/index.php
-cp dlsend.php /root/dlsend.php
+echo "${GREEN}Choose an option:${NC}"
+echo "${GREEN}1. Install Telegram server${NC}"
+echo "${GREEN}2. Install Telegram bot${NC}"
+read -p "${GREEN}Enter your choice (1 or 2): ${NC}" option
 
-# Change directory to the root directory
-cd /root/
+case $option in 
+    1)
+        echo "${GREEN}Installing ngrok .... ${NC}"
+        install_ngrok
 
-# Download and install ngrok
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz && \
-tar -xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin && \
-rm -rf ngrok-v3-stable-linux-amd64.tgz
+        # Set up Telegram bot token
+        read -p "Enter telegram bot token: " bot_token
+        export botToken="$bot_token"
 
-# Set up Telegram bot token
-read -p "Enter telegram bot token: " bot_token
-export botToken="$bot_token"
+        # Set up ngrok authtoken
+        read -p "Enter ngrok token: " nToken
+        ngrok config add-authtoken $nToken
 
-# Set up ngrok authtoken
-read -p "Enter ngrok token: " nToken
-ngrok config add-authtoken $nToken
+        # Start Docker container for Telegram bot API
+        echo -e "${GREEN}Starting Telegram Server...${NC}"
+        docker run -d -p 8081:8081 --name=telegram-bot-api --restart=always \
+            -v telegram-bot-api-data:/var/lib/telegram-bot-api \
+            -e TELEGRAM_API_ID=7784110 \
+            -e TELEGRAM_API_HASH=f81b6478f985c1283fa8c4847d1860ec \
+            -e TELEGRAM_LOCAL=1 \
+            -e TELEGRAM_STAT=1 \
+            aiogram/telegram-bot-api:latest
+        sleep 5
+        echo -e "${GREEN}Telegram server initialized...${NC}"
+        # Set ngrok static domain
+        read -p "${GREEN}Enter ngrok static domain: ${NC}" domain
+        read -p "${GREEN}Enter Telegram bot Backend endpoint: ${NC}" endpoint
+        # Set webhook for Telegram bot
+        echo -e "${GREEN}Setting webhook for Telegram bot...${NC}"
+        curl "https://$domain/bot$botToken/setWebhook?url=https://$endpoint/"
 
-# Start Docker container for Telegram bot API
-echo -e "${GREEN}Starting Docker container for Telegram bot API...${NC}"
-docker run -d -p 8081:8081 --name=telegram-bot-api --restart=always \
-    -v telegram-bot-api-data:/var/lib/telegram-bot-api \
-    -e TELEGRAM_API_ID=7784110 \
-    -e TELEGRAM_API_HASH=f81b6478f985c1283fa8c4847d1860ec \
-    -e TELEGRAM_LOCAL=1 \
-    -e TELEGRAM_STAT=1 \
-    -p 8082:8082 aiogram/telegram-bot-api:latest
+        # Sleep to allow Docker container to initialize
+        echo -e "${GREEN}Forwarding porting using ngrok ${NC}"
 
-# Set ngrok static domain
-read -p "Enter ngrok static domain: " domain
+        echo -e "${GREEN}Telegram server started at https://$domain/ ${NC}"
+        sleep 2
 
-# Set webhook for Telegram bot
-echo -e "${GREEN}Setting webhook for Telegram bot...${NC}"
-curl "http://localhost:8081/bot$botToken/setWebhook?url=https://$domain/"
-echo ""
+        ngrok http --domain=$domain http://localhost:8081
+        ;;
+    2)
+        apk add php php-curl php-dom php-openssl python3 ffmpeg
 
-# Sleep to allow Docker container to initialize
-echo -e "${GREEN}Waiting for Docker container to initialize...${NC}"
-sleep 10
+        # Download and install ngrok
+        install_ngrok
 
-# Start PHP server and ngrok
-echo -e "${GREEN}Starting PHP server and ngrok...${NC}"
-php -S localhost:8080 & \
-ngrok http --domain=$domain http://localhost:8080
+        # Set up Telegram bot token
+        read -p "Enter telegram bot token: " bot_token
+        export botToken="$bot_token"
+
+        # Set up ngrok authtoken
+        read -p "Enter ngrok token: " nToken
+        ngrok config add-authtoken $nToken
+
+        read -p "${GREEN}Enter ngrok static domain: ${NC}" domain
+        # Start PHP server and ngrok
+        echo -e "${GREEN}Starting PHP server and Initializing Ngrok...${NC}"
+        sleep 5
+        echo -e "${GREEN}Telegram bot server started at https://$domain/${NC}"
+        php -S localhost:8080 & \
+        ngrok http --domain=$domain http://localhost:8080
+        ;;
+    *)
+        echo "Invalid option. Please choose either 1 or 2."
+        ;;
+esac
